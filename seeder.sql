f71cb4f2ff98958fe68ea653856aa66df0efbaba
Kyle Maulsby <kylemaulsby15@gmail.com>
2019-09-19 16:56:06 -0500

    finish database

--- seeder.sql
+++ seeder.sql
@@ -1,4 +1,9 @@
 Use silver_lining_db;
+
+Drop TABLE if exists reviews;
+Drop TABLE if exists posts;
+Drop TABLE if exists users;
+
 # Users
 insert into Users (username, password, email, address, photo, role)
 values ('Bev Ivan', 'EWqlqmgPB0', 'bivan0@last.fm', '16 Granby Way', 'https://robohash.org/sintbeataeid.png?size=50x50&set=set1', 2),
