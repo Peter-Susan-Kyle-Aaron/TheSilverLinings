fea2237d7bf0347ed1bdbfcc4a1b10ae8861b4dd
susanlin0705 <susan.lin0705@gmail.com>
2019-09-23 15:14:40 -0500

    added map

---
+++ src/main/resources/static/js/keys.js
@@ -0,0 +1,5 @@
+var mapboxToken="pk.eyJ1IjoiZmFpeXRlZCIsImEiOiJjanhhdmhwbnkxMWM5M25ucnFka21xOXRzIn0.c_ImXwEydAQDNZH9nhk20A";
+
+var darkSkyeKey="8750add9758ed64b1ae670cb631afb76";
+
+var githubkey = "0a26f6defff2b5b2436aeaa38fe89fd78a125d0e";
