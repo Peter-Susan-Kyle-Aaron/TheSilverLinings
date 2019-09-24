3c5ff574e3a4845b5712edcef4378eff5f8f768d
Kyle Maulsby <kylemaulsby15@gmail.com>
2019-09-24 15:41:49 -0500

    recurrence

--- src/main/resources/static/js/keys.js
+++ src/main/resources/static/js/keys.js
@@ -3,3 +3,5 @@ var mapboxToken="pk.eyJ1IjoiZmFpeXRlZCIsImEiOiJjanhhdmhwbnkxMWM5M25ucnFka21xOXRz
 var darkSkyeKey="8750add9758ed64b1ae670cb631afb76";
 
 var githubkey = "0a26f6defff2b5b2436aeaa38fe89fd78a125d0e";
+
+var fileKey = "ABhuFU86wTyutrWvOsxZJz";
