197a8a57f4d33ac009277bd1398c093fdc9fd8d8
susanlin0705 <susan.lin0705@gmail.com>
2019-09-30 09:42:56 -0500

    showing current map location

---
+++ src/main/java/com/codeup/silverlining/Model/Location.java
@@ -0,0 +1,25 @@
+//package com.codeup.silverlining.Model;
+//
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "Locations")
+//public class Location {
+//
+//    @Id @GeneratedValue
+//    private long id;
+//
+//    @Column(name = "latitude")
+//    private String latitude;
+//
+//    @Column(name = "longitute")
+//    private String longitude;
+//
+//    @ManyToOne
+//    private Post post;
+//
+//
+//    public Location (long id, String latitude, String longitude){}
+//
+//}
