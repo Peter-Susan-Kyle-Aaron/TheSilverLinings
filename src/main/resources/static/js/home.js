b269767e67d683b8f555e1435476a4a99c83c70a
susanlin0705 <susan.lin0705@gmail.com>
2019-10-06 11:24:14 -0500

    origin susan

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
+
     $('.slider').slider({
         height: 600
     });
+
+
+    $('.slider').height(600);
+
+    $('.sidenav').sidenav();
+
+
+    setInterval(function(){
+        $('.slider').slider('next');
+    }, 5000);
+
+
+
 });
 
-
-setInterval(function(){
-    $('.slider').slider('next');
-}, 5000);
 
 
 
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.slider').height(600);
+
+    $('.sidenav').sidenav();
+
+});
